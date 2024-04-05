36c452b7c8b087d60b5bc6a8b400d801b47e549f
Bhargav Satya Sai <bhargavs@vconstruct.in>
2024-04-05 17:10:39 +0530

    add cart related models in cartapi project

---
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Data;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ShoppingCartAPI.Controllers
+{
+    [Route("api/cart")]
+    [ApiController]
+    public class CartAPIController : Controller
+    {
+       private ResponseDto _responseDto;
+        private IMapper _mapper;
+        private readonly AppDbContext _db;
+
+        public CartAPIController(AppDbContext db,
+            IMapper mapper)
+        {
+                _db = db;
+            this._responseDto = new ResponseDto();
+            _mapper = mapper;
+        }
+
+        [HttpPost("CartUpsert")]
+        public async Task<ResponseDto> CartUpsert(CartDto cartDto)
+        {
+
+        }
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/Migrations/20240405112836_AddshoppingCartTables.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mango.Services.ShoppingCartAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddshoppingCartTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CartHeaderDto",
+                columns: table => new
+                {
+                    CartHeaderId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CouponId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Discount = table.Column<double>(type: "float", nullable: false),
+                    CartTotal = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartHeaderDto", x => x.CartHeaderId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CartHeaders",
+                columns: table => new
+                {
+                    cartHeaderId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CouponId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartHeaders", x => x.cartHeaderId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CartDetails",
+                columns: table => new
+                {
+                    CartDetailsId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CartHeaderId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    count = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartDetails", x => x.CartDetailsId);
+                    table.ForeignKey(
+                        name: "FK_CartDetails_CartHeaderDto_CartHeaderId",
+                        column: x => x.CartHeaderId,
+                        principalTable: "CartHeaderDto",
+                        principalColumn: "CartHeaderId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartDetails_CartHeaderId",
+                table: "CartDetails",
+                column: "CartHeaderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CartDetails");
+
+            migrationBuilder.DropTable(
+                name: "CartHeaders");
+
+            migrationBuilder.DropTable(
+                name: "CartHeaderDto");
+        }
+    }
+}

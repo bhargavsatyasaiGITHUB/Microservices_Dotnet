657c827452797dbde7c7a21794b75b100b5e7dc6
Bhargav Satya Sai <bhargavs@vconstruct.in>
2024-04-05 15:17:46 +0530

    Make all products visible in Home view and for view details of prodcuts we need to login

---
+++ Mango.Services.ProductAPI/Controllers/ProductApiController.cs
@@ -0,0 +1,133 @@
+﻿using AutoMapper;
+
+using Mango.Services.ProductAPI.Data;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    // No need to Authorize to view list of Items
+   // [Authorize]
+    public class ProductApiController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private ResponseDto _responseDto;
+        private IMapper _mapper;
+
+        public ProductApiController(AppDbContext db,IMapper mapper)
+        {
+            _db = db;
+            _responseDto = new ResponseDto();
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Product> objList=_db.Products.ToList();
+                _responseDto.Result = _mapper.Map<IEnumerable<ProductDto>>(objList);
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess=false;
+                _responseDto.Message=ex.Message;
+                
+            }
+            return _responseDto;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Product objList = _db.Products.First(u=>u.ProductId==id);
+                _responseDto.Result= _mapper.Map<ProductDto>(objList);
+               
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+
+            }
+            return _responseDto;
+        }
+
+
+       
+
+
+        [HttpPost]
+        [Authorize(Roles ="ADMIN")]
+        public ResponseDto Post([FromBody] ProductDto ProductDto )
+        {
+            try
+            {
+                Product obj = _mapper.Map<Product>(ProductDto);
+                _db.Products.Add(obj);
+                _db.SaveChanges();
+                _responseDto.Result = _mapper.Map<ProductDto>(obj);
+
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+
+            }
+            return _responseDto;
+        }
+
+        [HttpPut]
+       [Authorize(Roles = "ADMIN")]
+        public ResponseDto Put([FromBody] ProductDto ProductDto)
+        {
+            try
+            {
+                Product obj = _mapper.Map<Product>(ProductDto);
+                _db.Products.Update(obj);
+                _db.SaveChanges();
+                _responseDto.Result = _mapper.Map<ProductDto>(obj);
+
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+
+            }
+            return _responseDto;
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        [Authorize(Roles = "ADMIN")]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                Product obj = _db.Products.First(u => u.ProductId == id);
+                _db.Products.Remove(obj);
+                _db.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+
+            }
+            return _responseDto;
+        }
+
+    }
+}
--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -1,23 +1,61 @@
 using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace Mango.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IProductService _productService;
+        public HomeController(IProductService productService)
         {
-            _logger = logger;
+            _productService = productService;
+        }
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public  async Task<IActionResult> Index()
+        {
+            List<ProductDto>? list = new();
+
+            ResponseDto? response = await _productService.GetAllProductAsync();
+
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return View(list);
         }
 
-        public IActionResult Index()
+        [Authorize]
+        public async Task<IActionResult> ProductDetails(int productId)
         {
-            return View();
-        }
+           ProductDto? model = new();
 
+            ResponseDto? response = await _productService.GetProductByIdAsync(productId);
+
+            if (response != null && response.IsSuccess)
+            {
+                model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return View(model);
+        }
         public IActionResult Privacy()
         {
             return View();
